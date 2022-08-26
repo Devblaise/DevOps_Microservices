bc9dc0f80692c692e59df61d5b38f0ae56127908
Ubuntu <ubuntu@ip-172-31-48-17.us-west-2.compute.internal>
2022-08-26 03:37:57 +0000

    chore: added repo files for project4

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="devblaise/microproject:v1"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run microproject --image=devblaise/microproject:v1 --port=80 --labels app=microproject
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward microproject 8000:80
